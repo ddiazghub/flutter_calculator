d6480c48209f100395af252ac59273f2c897a9fe
ddiazghub <dedemoya@uninorte.edu.co>
2023-09-25 14:28:17 -0500

    Modified server

--- server/main.py
+++ server/main.py
@@ -80,6 +80,7 @@ async def login(data: LoginSchema):
         "access_token": access_token,
         "refresh_token": refresh_token,
         "token_type": "bearer",
+        "user": user.model_dump(exclude={"password"})
     }
 
 
